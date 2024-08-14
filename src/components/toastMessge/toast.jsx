8ac9ef3f07d406c38b0775e27e0ebb726382fe81
Deepak4617 <dk4803382@gmail.com>
2024-08-14 17:39:43 +0530

    add

--- src/components/toastMessge/toast.jsx
+++ src/components/toastMessge/toast.jsx
@@ -3,6 +3,7 @@ import { Toast } from "flowbite-react";
 
 export const ToastMessage = ({message}) => {
   return (
+    
     <Toast>
     <div className="ml-3 text-sm font-normal">{message}</div>
     <Toast.Toggle />
